d43c25c1d9fc1f2b70cb586c080a692f296c24e4
lomovaaa <61076878+lomovaaa@users.noreply.github.com>
2021-04-15 20:22:58 +0300

    Удален проект со второй лабораторной

--- laba2/src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-import expressionCalculator.ExpressionParser;
-
-public class Main {
-    public static void main(String[] args) {
-        ExpressionParser parser = new ExpressionParser();
-        System.out.println("Введите выражение: ");
-    }
-}
