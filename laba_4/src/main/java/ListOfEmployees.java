d4db5f49987eebcbe621d0c8d90e7a069199ff89
LomovaAA <anastlomova14@gmail.com>
2021-04-26 21:12:39 +0300

    Реализация чтения данных из файла приведена в соответствии с примером. Добавлен метод для создания сотрудника и подразделения

--- laba_4/src/main/java/ListOfEmployees.java
+++ laba_4/src/main/java/ListOfEmployees.java
@@ -1,42 +1,69 @@
+import au.com.bytecode.opencsv.CSVReader;
+import java.io.*;
+import java.util.ArrayList;
+
 import models.Department;
 import models.Employee;
-
-import com.opencsv.CSVParser;
-import com.opencsv.CSVParserBuilder;
-import com.opencsv.CSVReader;
-import com.opencsv.CSVReaderBuilder;
-
-import java.io.*;
-import java.util.*;
 
 /**
  * Класс, реализующий формирование списка сотрудников.
  */
 public class ListOfEmployees {
-    private int departmentId = 0;
+    /**
+     * Возвращает подразделение для сотрудника.
+     * Если подразделение с указанным id уже существует, то возвращает его, иначе - создает новое.
+     *
+     * @param departments список существующих подразделений
+     * @param id id подразделения
+     * @param name название подразделения
+     * @return подразделение
+     */
+    private static Department getDepartment(ArrayList<Department> departments, int id, String name) {
+        for (Department department : departments) {
+            if (department.getId() == id) {
+                return department;
+            }
+        }
+        return new Department(name, id);
+    }
 
-    public void createList(String path, char separator) throws Exception {
+    /**
+     * Создает нового сотрудника по указанным данным, полученным из файла.
+     * @param departments список подразделений
+     * @param departmentId id подразделения
+     * @param data данные, полученные из файла
+     * @return сотрудник
+     * @throws Exception
+     */
+    private static Employee createEmployee(ArrayList<Department> departments, int departmentId, String[] data)
+            throws Exception {
+        return new Employee(data, getDepartment(departments, departmentId, data[4]));
+    }
+
+    /**
+     * Формирует из данных файла список сотрудников и возвращает его.
+     * @param fileName имя файла
+     * @param separator разделитель
+     * @return список сотрудников
+     * @throws Exception
+     */
+    public static ArrayList<Employee> createList(String fileName, char separator) throws Exception {
         ArrayList<Employee> employees = new ArrayList<Employee>();
         ArrayList<Department> departments = new ArrayList<Department>();
-
-        try {
-            FileReader reader = new FileReader(path);
-            CSVParser parser = new CSVParserBuilder().withSeparator(separator).build();
-            CSVReader csvReader = new CSVReaderBuilder(reader).withCSVParser(parser).build();
-            if (csvReader == null) {
-                throw new FileNotFoundException(path);
+        int departmentId = 0;
+        try (InputStream in = Main.class.getClassLoader().getResourceAsStream(fileName);
+             CSVReader reader = in == null ? null : new CSVReader(new InputStreamReader(in), separator)) {
+            if (reader == null) {
+                throw new FileNotFoundException(fileName);
             }
             String[] nextLine;
-            nextLine = csvReader.readNext();
-            while ((nextLine = csvReader.readNext()) != null){
-                if (nextLine != null){
-                    System.out.println(nextLine);
-                }
+            reader.readNext();
+            while ((nextLine = reader.readNext()) != null) {
+                employees.add(createEmployee(departments, departmentId++, nextLine));
             }
-
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        catch(IOException ex){
-            System.out.println(ex.getMessage());
-        }
+        return employees;
     }
 }
--- laba_4/src/main/java/Main.java
+++ laba_4/src/main/java/Main.java
@@ -1,13 +1,13 @@
+import java.util.ArrayList;
+
 import models.Employee;
 
-import java.io.IOException;
-import java.util.ArrayList;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        ListOfEmployees listEmp = new ListOfEmployees();
+    public static void main(String[] args) throws Exception {
         ArrayList<Employee> list = new ArrayList<Employee>();
-//        listEmp.createList("./src/main/resources/foreign_names.csv", ';');
+        list = ListOfEmployees.createList("foreign_names.csv", ';');
+        System.out.println(list);
     }
 }
